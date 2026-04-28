1d669cfa06f41e7cf3295ea5bc5d29a9589660ea
aleksi2105 <aleksi2105@yandex.ru>
2026-04-28 04:34:27 +0300

    add: types of variables

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-let title;
-let screens;
-let screenPrice;
-let rollBack;
-let fullPrice;
-let adaptive;
+let title = "Homeworks";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 550;
+let rollBack = 25;
+let fullPrice = 75055854;
+let adaptive = true;
 
-alert("Проверка связи")
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log("Привет, консоль!");
+console.log(screens.length);
 
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей/долларов/гривен/юани");
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "рублей/долларов/гривен/юани");
+
+screens = screens.toLowerCase();
+
+console.log(screens.split(", "));
+
+console.log("Процент отката посреднику за работу" + " " + fullPrice * (rollBack / 100));
