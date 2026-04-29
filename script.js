c20a2a2d0b1563d879bd4409f3934ad1cdcaa151
aleksi2105 <aleksi2105@yandex.ru>
2026-04-29 04:49:55 +0300

    add: lesson03

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let title = "Homeworks";
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 550;
@@ -19,3 +21,31 @@ screens = screens.toLowerCase();
 console.log(screens.split(", "));
 
 console.log("Процент отката посреднику за работу" + " " + fullPrice * (rollBack / 100));
+
+title = prompt("Как называется ваш проект?");
+
+screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+
+screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
+
+adaptive = confirm("Нужен ли адаптив на сайте?")
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollBack / 100)));
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice >= 0) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что то пошло не так");
+}
